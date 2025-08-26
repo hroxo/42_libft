da6e472abd3b5f21d633ac16cbf04c422e6d02d3
hroxo <hugo.roxo04@gmail.com>
2025-08-26 22:56:19 +0100

    added inicial functions to libft

--- libft/libft.h
+++ libft/libft.h
@@ -6,13 +6,21 @@
 /*   By: hroxo <hroxo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 19:31:51 by hroxo             #+#    #+#             */
-/*   Updated: 2025/08/26 19:31:51 by hroxo            ###   ########.fr       */
+/*   Updated: 2025/08/26 21:11:57 by hroxo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int	isalpha(int c);
+# include <stdlib.h>
+
+size_t	ft_strlen(const char *s);
+void	*memset(void *s, int c, size_t n);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
 
 #endif
