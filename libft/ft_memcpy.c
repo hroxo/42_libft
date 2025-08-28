a0b1f83d938dc0c434575194114fabd3e79867e1
hroxo <hugo.roxo04@gmail.com>
2025-08-28 11:00:17 +0100

    mod makefile libft

---
+++ libft/ft_memcpy.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hroxo <hroxo@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/27 23:41:35 by hroxo             #+#    #+#             */
+/*   Updated: 2025/08/27 23:57:46 by hroxo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t				i;
+	const unsigned char	*pstr;
+	unsigned char		*pdest;
+
+	pstr = (const unsigned char *)src;
+	pdest = (unsigned char *)dest;
+	i = 0;
+	while (i < n)
+	{
+		pdest[i] = pstr[i];
+		i++;
+	}
+	return (dest);
+}
+/*
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char	src[10] = "Ola miguel";
+	char	dest[29] = "burro que es obrigado";
+	printf("==>Minha funcao<==\n");
+	printf("%s\n", (char *)ft_memcpy((void *)dest, (void *)src, 3));
+	printf("==>Funcao Original<==\n");
+	printf("%s\n", (char *)memcpy((void *)dest, (void *)src, 3));
+}
+*/
--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -1,28 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hroxo <hroxo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/26 19:31:51 by hroxo             #+#    #+#             */
-/*   Updated: 2025/08/27 23:58:09 by hroxo            ###   ########.fr       */
+/*   Created: 2025/08/28 00:01:59 by hroxo             #+#    #+#             */
+/*   Updated: 2025/08/28 00:14:21 by hroxo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include <stddef.h>
+#include <stdlib.h>
 
-# include <stdlib.h>
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+	unsigned char temp_buffer[n];
+	unsigned char	*p_dest;
+	unsigned char	*p_src;
 
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-size_t	ft_strlen(const char *s);
-void	*memset(void *s, int c, size_t n);
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-
-#endif
+	p_dest = (unsigned char *)dest;
+	p_src = (unsigned char *)src;
+	i = 0;
+	while (i < n)
+	{
+		temp_buffer[i] = p_src[i];
+		p_dest[i] = temp_buffer[i];
+		i++;
+	}
+	return (dest);
+}
