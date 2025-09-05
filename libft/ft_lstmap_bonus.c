f93ef4e603e80e6dfa0a7defcb2907fae13cb1ce
hroxo <hugo.roxo04@gmail.com>
2025-09-05 11:34:01 +0100

    added final and norminetted

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "libft_bonus.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
